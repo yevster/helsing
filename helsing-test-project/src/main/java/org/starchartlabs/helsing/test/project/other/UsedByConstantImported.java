7444e155aca9bf7a97dffcadf38ebf656e5d41ff
Ryan O'Meara <romeara@live.com>
2020-02-06 01:45:48 -0500

    Add remaining common test cases for basic class use detection

---
+++ helsing-test-project/src/main/java/org/starchartlabs/helsing/test/project/other/UsedByConstantImported.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) Feb 6, 2020 StarChart Labs Authors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    romeara - initial API and implementation and/or initial documentation
+ */
+package org.starchartlabs.helsing.test.project.other;
+
+/**
+ * This class is used to test detection via use of a constant within a different package when the imported and
+ * referenced by simple name
+ *
+ * @author romeara
+ */
+public class UsedByConstantImported {
+
+    public static final String CONSTANT = "constant";
+
+}
